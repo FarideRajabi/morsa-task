5be0f4d7d61bae0967938f10ebb6068c4bd5f067
Farideh Rajabi <faridehrajabi1997@gmail.com>
2023-09-28 14:39:35 +0330

    edit functionality

--- src/components/webcamForm.jsx
+++ src/components/webcamForm.jsx
@@ -1,41 +1,58 @@
-import { useState, useEffect, useCallback } from "react";
+import { useRef, useCallback, useState } from "react";
 import Webcam from "react-webcam";
-import { Col, Form, Row } from "react-bootstrap";
+import { Row, Col, Form } from "react-bootstrap";
 import { BsFillCameraFill } from 'react-icons/bs';
+import { AiOutlineClose } from "react-icons/ai";
+
 
 const WebcamForm = ({ label }) => {
+
     const [showCamera, setShowCamera] = useState(false);
-    const [devices, setDevices] = useState([]);
+    const [imgSrc, setImgSrc] = useState(null);
 
-    const handleDevices = useCallback(
-        mediaDevices =>
-            setDevices(mediaDevices.filter(({ kind }) => kind === "videoinput")),
-        [setDevices]
-    );
+    const webcamRef = useRef(null);
 
-    useEffect(
-        () => {
-            navigator.mediaDevices.enumerateDevices().then(handleDevices);
-        },
-        [handleDevices]
-    );
+    const capture = useCallback(() => {
+        const imageSrc = webcamRef.current.getScreenshot();
+        setImgSrc(imageSrc);
+    }, [webcamRef]);
+
+    const retake = () => {
+        setImgSrc(null);
+    };
 
     return (
-        <Row>
-            <Col className="col-3" style={{ textAlign: "right" }}>
-                <Form.Label className="col-4 h6">{label}:</Form.Label>
-            </Col>
-            <Col className="col-2">
-                <BsFillCameraFill type="button" className="h3 mt-1" onClick={() => { setShowCamera(!showCamera) }} />
-                {devices.map((device) => (
-                    <div>
-                        {showCamera && <Webcam audio={false} videoConstraints={{ deviceId: device.deviceId }} />}
-                    </div>
-                ))}
-            </Col>
-            <Col className="col-6"></Col>
-        </Row>
-    );
-};
+        <>
+            <Row className="container">
+                <Col className="col-3" style={{ textAlign: "right" }}>
+                    <Form.Label className="h6">{label}:</Form.Label>
+                </Col>
+                <Col className="col-1">
+                    {!showCamera && <BsFillCameraFill type="button" onClick={() => setShowCamera(!showCamera)} className="h1" />}
+                    {!!showCamera && <BsFillCameraFill type="button" onClick={capture} className="h1" />}
 
-export default WebcamForm
+                </Col>
+                <Col className="col-8">
+                    {!!imgSrc && (<>
+                        <img src={imgSrc} alt="webcam" className="w-50" />
+                        <AiOutlineClose type="button" onClick={retake} className="mb-5" />
+                    </>
+                    )}
+                </Col>
+            </Row>
+            <div>
+                {!!showCamera && !imgSrc && <div className="mx-auto stiky-top text-center">
+                    <Webcam
+                        height={400}
+                        width={500}
+                        ref={webcamRef}
+                        screenshotFormat="image/jpeg"
+                    />
+                </div>}
+            </div>
+        </>
+    );
+
+}
+
+export default WebcamForm;
